4a24ba72b12930ab4a045c48bf11599254bedfab
Taufiq Agung Kurniawan <taufiqagung23@gmail.com>
2025-06-25 15:11:34 +0700

    Add file Dashboard Susenas

---
+++ DashboardSusenas.py
@@ -0,0 +1,9 @@
+# Dashboard Monitoring Pengolahan Susenas Maret 2025
+# BPS Kabupaten Dharmasraya
+# Dalam Rangka Pelaksanaan Aktualisasi Latsar CPNS BPS 2025
+# Taufiq Agung Kurniawan - 200204232024121002
+
+# 0. Import Library
+import streamlit as st
+
+print("Hello World")
