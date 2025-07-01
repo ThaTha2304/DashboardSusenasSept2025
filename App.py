4e4c64fdbfada2927dbd346ea25e2f81c459a9b1
ThaTha2304 <taufiqagung23@gmail.com>
2025-07-01 11:43:00 +0700

    Modifikasi pada file app.py, tambah jadi multipages

--- App.py
+++ App.py
@@ -3,17 +3,24 @@
 # Dalam Rangka Pelaksanaan Aktualisasi Latsar CPNS BPS 2025
 # Taufiq Agung Kurniawan - 200204232024121002
 
-# 0. Import Library
 import streamlit as st
-from streamlit_gsheets import GSheetsConnection
 
-# Connection to Google Sheets
-conn = st.connection("gsheets", type=GSheetsConnection)
+st.set_page_config(layout="wide")
 
-df = conn.read(
-    worksheet="Updating",
-    ttl = 1
-)
+# Define pages
+dash_updating = st.Page("DashUpdating.py", title="Updating", icon="📝")
+dash_pendataan = st.Page("DashPendataan.py", title="Pendataan", icon="📊")
+dash_petugas = st.Page("DashPetugas.py", title="Rekapitulasi Petugas", icon="😃")
 
-# Print results
-st.dataframe(df)
+# Set up navigation (Section)
+pg = st.navigation({
+    "Pengolahan": [dash_updating, dash_pendataan],
+    "Rekap Petugas" : [dash_petugas]
+})
+st.logo("assets\img\logo-bps-oke.png", size="large")
+st.title("Dashboard Pengolahan Susenas Maret 2025")
+st.subheader("BPS Kabupaten Dharmasraya")
+
+st.sidebar.button("Reset")
+
+pg.run()
