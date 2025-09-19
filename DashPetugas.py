ae382b093e4728462d57630c1c3d4800c4406ee0
Taufiq Agung Kurniawan <taufiqagung23@gmail.com>
2025-09-19 17:46:48 +0700

    Hapus bagian rekap updating

--- DashPetugas.py
+++ DashPetugas.py
@@ -3,12 +3,12 @@ import plotly.graph_objects as go
 import myconfig.Connection as Connection
 
 # Define data
-updatingData = Connection.getDataUpdating()
+# updatingData = Connection.getDataUpdating()
 pendataanData = Connection.getDataPendataan()
 
-rekap_updating = updatingData["Nama Pengentri"].value_counts().reset_index()
-rekap_updating.columns = ["Nama Pengentri", "Jumlah Dokumen"]
-rekap_updating = rekap_updating.sort_values(by = "Jumlah Dokumen")
+# rekap_updating = updatingData["Nama Pengentri"].value_counts().reset_index()
+# rekap_updating.columns = ["Nama Pengentri", "Jumlah Dokumen"]
+# rekap_updating = rekap_updating.sort_values(by = "Jumlah Dokumen")
 
 rekap_pendataan = pendataanData["Nama Pengentri"].value_counts().reset_index()
 rekap_pendataan.columns = ["Nama Pengentri", "Jumlah Dokumen"]
@@ -16,32 +16,32 @@ rekap_pendataan = rekap_pendataan.sort_values(by = "Jumlah Dokumen")
 
 col1, col2 = st.columns([1,1], border = True)
 
-with col1:
-    st.markdown("#### Rekapitulasi Updating Susenas")
-    st.markdown("##### Entri Dokumen Updating")
+# with col1:
+#     st.markdown("#### Rekapitulasi Updating Susenas")
+#     st.markdown("##### Entri Dokumen Updating")
 
-    figUpdating = go.Figure(
-        data = [go.Bar(
-            x = rekap_updating["Jumlah Dokumen"],
-            y = rekap_updating["Nama Pengentri"],
-            orientation = "h",
-            text = rekap_updating["Jumlah Dokumen"],
-            textposition = "inside",
-            insidetextanchor = "middle",
-            textfont = dict(
-                size = 14
-            )
-        )]
-    )
+#     figUpdating = go.Figure(
+#         data = [go.Bar(
+#             x = rekap_updating["Jumlah Dokumen"],
+#             y = rekap_updating["Nama Pengentri"],
+#             orientation = "h",
+#             text = rekap_updating["Jumlah Dokumen"],
+#             textposition = "inside",
+#             insidetextanchor = "middle",
+#             textfont = dict(
+#                 size = 14
+#             )
+#         )]
+#     )
 
-    figUpdating.update_layout(
-        xaxis_title = "Jumlah Dokumen",
-        yaxis_title = "Nama Pengentri",
-        template = "plotly_white",
-        margin = dict(l=10, r=10, t=0, b=0),
-    )
+#     figUpdating.update_layout(
+#         xaxis_title = "Jumlah Dokumen",
+#         yaxis_title = "Nama Pengentri",
+#         template = "plotly_white",
+#         margin = dict(l=10, r=10, t=0, b=0),
+#     )
 
-    st.plotly_chart(figUpdating)
+#     st.plotly_chart(figUpdating)
 
 with col2:
     st.markdown("#### Rekapitulasi Pendataan Susenas")
